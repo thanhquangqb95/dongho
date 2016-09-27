dfcd0034ce9c999e95fbffabf6f3405830debea6
Lê Thanh Quảng <thanhquangqb95@gmail.com>
2016-09-27 21:04:28 +0700

    phiên bản 1.0
    chưa có database.

---
+++ DongHo/DongHo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DongHo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DongHo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SanPham()
+        {
+            return View();
+
+        }
+        public ActionResult SanPhamNu()
+        {
+            var SanPham = new List<SanPhamNu>();
+            SanPham.Add(new SanPhamNu(1, "$223", "Rolex1", "/image/dongho/DH1.2.jpg"));
+            SanPham.Add(new SanPhamNu(2, "$223", "Rolex1", "/image/dongho/DH1.2.jpg"));
+            SanPham.Add(new SanPhamNu(3, "$223", "Rolex1", "/image/dongho/DH1.2.jpg"));
+            return View(SanPham);
+        }
+    
+    }
+   
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# dongho
-Web DongHo
